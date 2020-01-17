8641bd2029c26e403b67d5ae3882789d7b1b325f
Nithiwat Seesillapachai <ns271@duke.edu>
2020-01-16 23:08:42 -0500

    first commit

---
+++ app/src/main/java/com/example/nithiwat/helloworld/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.nithiwat.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimaryDark));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorLightBlue));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newText = ((EditText) findViewById(R.id.editText)).getText().toString();
+
+                if (TextUtils.isEmpty(newText)) {
+                    ((TextView) findViewById(R.id.textView)).setText("Text Is Empty");
+                } else {
+                    ((TextView) findViewById(R.id.textView)).setText(newText);
+                }
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.textView)).setText("Hello From Ter");
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.background_light));
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+
+    }
+}
